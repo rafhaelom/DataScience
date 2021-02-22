9f110a230f194c2c32ac5a333021e3fec7319a88
rafhaelom <rafha-1012@hotmail.com>
2021-02-21 22:36:57 -0300

    :construction: add: new script

--- Kaggle/previsaobrentoil.py
+++ Kaggle/previsaobrentoil.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 # %% Importando dados
-df = pd.read_csv("BrentOilPrices.csv", sep=",")
+df = pd.read_csv("dados/BrentOilPrices.csv", sep=",")
 # %% Análise Exploratória
 df.info()
 # %%
